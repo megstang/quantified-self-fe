1b547c65214b5b43bfd6c28c010c171360d0772d
Meghan Stang <mstang15@gmail.com>
2019-02-06 17:16:19 -0700

    Change environment variables

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
